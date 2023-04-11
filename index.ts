29be175df92dd751cff7e47ae01f851fa627319e
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2023-04-11 10:27:41 +0000

    feat: :tada: first-commit

---
+++ index.ts
@@ -0,0 +1,17 @@
+import { FmFrontend } from "./services/frontend"
+import { FmBackend } from "./services/backend"
+
+function main() {
+    
+    new FmBackend({
+        Name: "example",
+        Product: "devops-course",
+    })
+
+    new FmFrontend({
+        Name: "example",
+        Product: "devops-course",
+    })
+}
+
+main()
---
+++ resources/bucket.ts
@@ -0,0 +1,59 @@
+import { ComponentResource, CustomResourceOptions ,getStack } from "@pulumi/pulumi"
+import { s3 } from "@pulumi/aws";
+
+type FmBucketArgs = {
+    Name: string;
+    Product: string;
+    Public: boolean;
+}
+
+export class FmBucket extends ComponentResource {
+    constructor(args: FmBucketArgs, opts?: CustomResourceOptions) {
+        const resourceName = `${args.Product}-${args.Name}`;
+        super("pkg:index:FmBucket", resourceName, {}, opts);
+        const stack = getStack();
+        const bucketName = `${resourceName}-${stack}`;
+
+        let bucketArgs: s3.BucketArgs = {
+            acl: "private",
+            bucket: bucketName,
+            tags: {
+                Environment: stack,
+            },
+
+        }
+
+        if(args.Public){
+            bucketArgs.acl = "public-read";
+            bucketArgs.website = {
+                indexDocument: "index.html",
+                errorDocument: "error.html",
+                routingRules: `[{
+                    "Condition": {
+                        "KeyPrefixEquals": "docs/"
+                    },
+                    "Redirect": {
+                        "ReplaceKeyPrefixWith": "documents/"
+                    }
+                }]
+                `,
+            }
+        }
+        const bucket = new s3.Bucket(args.Name, bucketArgs, {
+            parent: this
+        });
+
+        if(!args.Public){
+            new s3.BucketPublicAccessBlock(args.Name, {
+                bucket: bucket.id,
+                blockPublicAcls: true,
+                blockPublicPolicy: true,
+                ignorePublicAcls: true,
+                restrictPublicBuckets: true,
+            }, {
+                parent: this
+            });
+        }
+       
+    }
+}
